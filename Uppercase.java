b6df22ec5c8adc45c54881e7fa955685cd961e5d
Sakethganakrishna-medidi <sakethganakrishna@gmail.com>
2025-01-29 12:52:40 +0530

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,8 @@
+public class Uppercase {
+    public static void main(String[] args) {
+        String a = "saketh";
+        String b = "SAKETH";
+        System.out.println(a.toUpperCase());
+        System.out.println(b.toLowerCase());
+    }
+}
